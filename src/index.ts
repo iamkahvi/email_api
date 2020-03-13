edad126a0a57d8d6e5a06583b2ab2568cfc0d29a
Kahvi Patel <iamkahvi@gmail.com>
2020-03-12 23:39:52 -0700

    adding email route

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
 import * as express from "express";
+import * as cors from "cors";
+import * as bodyParser from "body-parser";
+
 const app = express();
+app.use(cors())
+app.use(bodyParser.json());
+
 const port = 3000;
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.post('/email', (req, res) => {
+    res.status(200).send(JSON.stringify({
+        foo: "bar"
+    }))
+    console.log(req.body);
+})
+
+
