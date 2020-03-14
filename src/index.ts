05a9d51f8aa0a5718ec7839d06264e9d41e12e21
Kahvi Patel <iamkahvi@gmail.com>
2020-03-14 16:57:20 -0700

    mailchimp api working

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import * as cors from "cors";
 import * as bodyParser from "body-parser";
 const md5 = require("md5");
 const fetch = require("node-fetch");
+require("dotenv").config();
 
 const app = express();
 
@@ -15,21 +16,34 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.post("/email", async (req, res) => {
-  res.status(200).send("subscription successful!");
-  console.log(req.body);
-  console.log(req.body.email);
-  const apiKey = "b6f30bb2eba093453b54fb9592bd1ea5-us19";
-  const listId = "b2503f99aa";
-  const md5Email = md5(req.body.email);
-  const url = `https://us19.api.mailchimp.com/3.0/lists/${listId}/members/${md5Email}`;
+  const apiKey = process.env.MAILCHIMP_API_KEY;
+  const listId = process.env.MAILCHIMP_LIST_ID;
+  const url = `https://us19.api.mailchimp.com/3.0/lists/${listId}/members/`;
+
   const response = await fetch(url, {
-    method: "GET",
+    method: "POST",
     headers: {
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "*",
-      "Authorization": `Basic ${apiKey}`
-    }
+      Authorization: `Basic ${apiKey}`
+    },
+    body: JSON.stringify({
+      email_address: req.body.email,
+      status: "subscribed"
+    })
   });
-  const data = await response.json()
-  console.log(data)
+
+  const data = await response.json();
+
+  console.log(data);
+
+  if (data.status === "subscribed") {
+    res.status(200).send(JSON.stringify({ title: "subscription successful!" }));
+  } else {
+    res
+      .status(500)
+      .send(
+        JSON.stringify({ title: data.title, description: data.description })
+      );
+  }
 });
