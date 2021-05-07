ff0292c68289ccf1b3bf4fe640843521fafd9460
Kahvi Patel <iamkahvi@gmail.com>
2021-05-06 20:04:48 -0700

    Removing unused imports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import * as express from "express";
 import * as cors from "cors";
-import * as bodyParser from "body-parser";
-const md5 = require("md5");
 const fetch = require("node-fetch");
 require("dotenv").config();
 
